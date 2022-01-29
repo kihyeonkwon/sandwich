75a57103c7116ca1e4707849541f53795594e037
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-01-29 15:26:17 +0900

    feat: Add models.py

--- sandwichapp/admin.py
+++ sandwichapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Bread, Topping, Cheese, Sauce, Sandwich
 
 # Register your models here.
+
+admin.site.register(Bread)
+admin.site.register(Topping)
+admin.site.register(Cheese)
+admin.site.register(Sauce)
+admin.site.register(Sandwich)
+
--- sandwichapp/models.py
+++ sandwichapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Bread(models.Model):
+    name = models.CharField(max_length=255)
+    inventory_count = models.IntegerField()
+    price = models.IntegerField()
+    def __str__(self):
+        return f'{self.name} 가격{self.price} 남은갯수{self.inventory_count}'
+
+class Topping(models.Model):
+    name = models.CharField(max_length=255)
+    inventory_count = models.IntegerField()
+    price = models.IntegerField()
+    def __str__(self):
+        return f'{self.name} 가격{self.price} 남은갯수{self.inventory_count}'    
+
+class Cheese(models.Model):
+    name = models.CharField(max_length=255)
+    inventory_count = models.IntegerField()
+    price = models.IntegerField()
+    def __str__(self):
+        return f'{self.name} 가격{self.price} 남은갯수{self.inventory_count}'
+
+class Sauce(models.Model):
+    name = models.CharField(max_length=255)
+    inventory_count = models.IntegerField()
+    price = models.IntegerField()
+    def __str__(self):
+        return f'{self.name} 가격{self.price} 남은갯수{self.inventory_count}'
+
+class Sandwich(models.Model):
+    bread = models.ForeignKey(Bread, on_delete=models.DO_NOTHING)
+    toppings = models.ManyToManyField(Topping)
+    cheese = models.ForeignKey(Cheese, on_delete=models.DO_NOTHING)
+    sauces = models.ManyToManyField(Sauce)
+
