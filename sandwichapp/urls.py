76dfd81a4b129277c3cf896d6dd22d9d0e2a4615
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-01-29 16:05:27 +0900

    feat: Add Bread CRUD in views.py

--- sandwichapp/urls.py
+++ sandwichapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('bread/', views.Bread.as_view()),
+    path('bread/', views.BreadView.as_view()),
     # path('topping/', views.Topping.as_view()),
     # path('cheese/', views.Cheese.as_view()),
     # path('sauce/', views.Sauce.as_view()),
--- sandwichapp/views.py
+++ sandwichapp/views.py
@@ -4,12 +4,33 @@ from sandwichapp.models import Bread, Topping, Cheese, Sauce, Sandwich
 from sandwichapp.serializers import BreadSerializer, ToppingSerializer, CheeseSerializer, SauceSerializer, SandwichSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status, generics
 
 
 
 
-class Bread(APIView):
+class BreadView(APIView):
     def get(self, request, format=None):
         breads = Bread.objects.all()
         serializer = BreadSerializer(breads, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BreadSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, format=None):
+        bread = generics.get_object_or_404(Bread, pk = request.data['id'])
+        serializer = BreadSerializer(bread, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, format=None):
+        bread = generics.get_object_or_404(Bread, pk = request.data['id']).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
