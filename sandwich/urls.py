da503bd7f3fb5198138a7b87ecfeda9adee945c8
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-01-30 10:11:24 +0900

    docs: Fix  requirements.txt

--- sandwich/urls.py
+++ sandwich/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls import url 
 from rest_framework import permissions 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
